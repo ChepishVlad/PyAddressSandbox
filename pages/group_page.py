a7cb7baca283c0d7967dbe9c1e4326fbe8718062
ChepishVlad <1Amuvyvape>
2021-06-27 21:14:19 +0300

    little update

--- pages/group_page.py
+++ pages/group_page.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.common.by import By
 
+from objects.group import Group
 from pages import BasePage
 
 
@@ -11,10 +12,7 @@ class GroupPage(BasePage):
     _footer_textarea = (By.NAME, 'group_footer')
     _submit_btn = (By.NAME, 'submit')
 
-    def fill_fields_and_submit(self,
-                               name: str,
-                               header: str,
-                               footer: str):
+    def fill_fields_and_submit(self, name: str, header: str, footer: str):
         name_input = self.find_element(self._name_input)
         name_input.clear()
         name_input.send_keys(name)
@@ -28,10 +26,7 @@ class GroupPage(BasePage):
     def press_submit_btn(self):
         self.find_element(self._submit_btn).click()
 
-    def create_new_group(self,
-                         name: str,
-                         header: str = '',
-                         footer: str = ''):
+    def create_new_group(self, group: Group):
         self.fill_fields_and_submit(
-            name=name, header=header, footer=footer)
+            name=group.name, header=group.header, footer=group.footer)
         self.press_submit_btn()
--- tests/ui_tests/test_login.py
+++ tests/ui_tests/test_login.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
+from objects.group import Group
 
 
 def test_login(login_page, groups_page, group_page):
+    group = Group(name='name', header='header', footer='footer')
     login_page.open_login_page()
     login_page.login('admin', 'secret')
     groups_page.open_groups_page()
     groups_before = groups_page.get_groups_list()
     groups_page.press_create_new_btn()
-    group_page.create_new_group(
-        name='name', header='header', footer='footer')
+    group_page.create_new_group(group)
     groups_page.open_groups_page()
     groups_after = groups_page.get_groups_list()
     assert len(groups_after) == len(groups_before) + 1
+
+
+def test_unsuccessful_login(login_page):
+    login_page.open_login_page()
+    login_page.login('admin', 'not_secret')
+    assert login_page.find_element(login_page._submit_btn).is_displayed(), (
+        'Кнопка логина не отображается')
